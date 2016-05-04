77e44ec3a8fdb5136bb4346d3ab0e4d393f23cd5
Boghdan Pavlov <lukures@yandex.ru>
2016-05-04 13:43:20 +0300

    server

---
+++ build/scripts/main.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+
+    $('.slidershow').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        prevArrow: $('.left'),
+        nextArrow: $('.right'),
+        cssEase: 'linear'
+    });
+
+
+});
+
